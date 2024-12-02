987d0b777183a07d6d6ed40bc74804b33da7a672
ElcioAngelo <AngeloNegri32@outlook.com>
2024-12-02 13:50:41 -0300

    sleep 30 segundos

--- flask/entrypoint.sh
+++ flask/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Run the tests first
-echo "Checking if mariadb container is up and available.."
-/app/wait-for-it.sh mariadb_container:3306 --timeout=10 --strict -- echo "MariaDB is up"
 echo "Running tests with pytest..."
 pytest app_test.py 
 
 # Check if pytest passed (exit code 0 means tests passed)
+sleep 30
 if [ $? -eq 0 ]; then
   echo "Tests passed. Starting Flask app..."
   # Run the Flask app
