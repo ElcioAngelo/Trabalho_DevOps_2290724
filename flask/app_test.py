1d28cd272371ca051836ff35bd4d0813a0e8af13
ElcioAngelo <AngeloNegri32@outlook.com>
2024-12-02 13:49:10 -0300

    disciplinas, no plural

--- flask/app_test.py
+++ flask/app_test.py
@@ -1,60 +1,3 @@
-# import time 
-# import requests
-
-# flask_app_url = 'http://localhost:5000'
-# register_user_url = 'http://localhost:5000/alunomodelview/add'
-# user_data = {
-#     'nome': 'UsuarioTeste',
-#     'sobrenome': 'sobreUusarioTeste',
-#     'turma': 'TurmaDoUsuarioTeste',
-#     'disciplina': 'DisciplinaTeste',
-# }
-
-# def is_container_running():
-    
-#     try:
-#         response = requests.get(flask_app_url)
-#         if response.status_code == 200:
-#             print("Container is up and running!")
-#             return True
-#         else:
-#             print("Container is not running...")
-#             return False
-#     except requests.exceptions.RequestException as e:
-#         print("An error has ocurred while checking container..")
-#         return False
-
-# def register_user():
-#     try:
-#         response = requests.post(register_user_url,user_data)
-#         if response.status_code == 200:
-#             print("User created with sucess!!")
-#             return True
-#         else:
-#             print("Failed to create User...")
-#             return False
-#     except requests.exceptions.RequestException as e:
-#         print("An error has occurred while creating the user..")
-#         return False 
-        
-        
-# def main():
-    
-#     retries = 5
-#     while retries > 0:
-#         if is_container_running():
-#             register_user()
-#             break
-#         else:
-#             retries -= 1
-#             print("Trying again..")
-#             time.sleep(5)
-#     else:
-#         print("Container failed to open.")
-        
-# if __name__ == "__main__":
-#     main()
-  
 import pytest
 import requests
 from unittest.mock import patch
@@ -66,11 +9,11 @@ def is_server_running(url: str) -> bool:
     except requests.exceptions.RequestException:
         return False
     
-def create_user(url: str, nome: str, sobrenome: str, matricula: str, turma: str):
+def create_user(url: str, nome: str, sobrenome: str, disciplinas: str, turma: str):
     user_data = {
         "nome": nome,
         "sobrenome": sobrenome,
-        "matricula": matricula,
+        "disciplinas": disciplinas,
         "turma": turma
     }
     response = requests.post(f"{url}/create_user", json=user_data)
