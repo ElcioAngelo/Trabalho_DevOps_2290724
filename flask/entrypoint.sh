fa7461cac827dc0de6c00b48de6413d1b998bcf9
ElcioAngelo <AngeloNegri32@outlook.com>
2024-12-02 13:44:52 -0300

    usando o waitforit para verificação

--- flask/entrypoint.sh
+++ flask/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Run the tests first
+echo "Checking if mariadb container is up and available.."
+/app/wait-for-it.sh mariadb_container:3306 --timeout=10 --strict -- echo "MariaDB is up"
 echo "Running tests with pytest..."
-pytest app_test.py --host=0.0.0.0
+pytest app_test.py 
 
 # Check if pytest passed (exit code 0 means tests passed)
 if [ $? -eq 0 ]; then
